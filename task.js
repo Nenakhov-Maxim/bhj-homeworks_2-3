7459d481e2dd542aab3ca11dd0929af6abe3ec01
Nenakhov.Max@yandex.ru <Nenakhov.Max@yandex.ru>
2021-02-08 08:52:56 +0500

    Исправление 3 задания

--- task.js
+++ task.js
@@ -1,41 +1,26 @@
 let clickLeft = document.getElementsByClassName('slider__navigation').item(0).querySelector(".slider__arrow_next");
 let clickRight = document.getElementsByClassName('slider__navigation').item(0).querySelector(".slider__arrow_prev");
+let sliderImagesArray = Array.from(document.getElementsByClassName('slider__item'));
 
 clickLeft.onclick = goToNext;
 clickRight.onclick = goToPrev;
 
 function goToNext() {
-    let sliderImage = document.getElementsByClassName('slider__image');
-    let arrayImage = Array.from(sliderImage);
-    for (let i = 0; i < arrayImage.length; i++) {
-        const element = arrayImage[i];
-        if (element.closest('div').className === "slider__item slider__item_active") {
-            element.closest('div').className = "slider__item";
-            if (i + 1 >= arrayImage.length) {
-                arrayImage[0].closest('div').className = "slider__item slider__item_active";
-            } else {
-                arrayImage[i + 1].closest('div').className = "slider__item slider__item_active";
-            }
-            return false;
-        }
-
-    }
+    let indexActiveSlide = sliderImagesArray.findIndex(searchActiveSlide);
+    sliderImagesArray[indexActiveSlide].classList.remove('slider__item_active');
+    let nextIndex = indexActiveSlide === sliderImagesArray.length - 1 ? 0 : indexActiveSlide + 1;
+    sliderImagesArray[nextIndex].classList.add('slider__item_active');
 }
 
 function goToPrev() {
-    let sliderImage = document.getElementsByClassName('slider__image');
-    let arrayImage = Array.from(sliderImage);
-    for (let i = arrayImage.length - 1; i >= 0; i--) {
-        const element = arrayImage[i];
-        if (element.closest('div').className === "slider__item slider__item_active") {
-            element.closest('div').className = "slider__item";
-            if (i - 1 < 0) {
-                arrayImage[arrayImage.length - 1].closest('div').className = "slider__item slider__item_active";
-            } else {
-                arrayImage[i - 1].closest('div').className = "slider__item slider__item_active";
-            }
-            return false;
-        }
+    let indexActiveSlide = sliderImagesArray.findIndex(searchActiveSlide);
+    sliderImagesArray[indexActiveSlide].classList.remove('slider__item_active');
+    let nextIndex = indexActiveSlide === 0 ? sliderImagesArray.length - 1 : indexActiveSlide - 1;
+    sliderImagesArray[nextIndex].classList.add('slider__item_active');
+}
 
+function searchActiveSlide(i) {
+    if (i.className === "slider__item slider__item_active") {
+        return i;
     }
 }
